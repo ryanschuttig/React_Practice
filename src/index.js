2d1b2a2b4967160e953e0b019c12b5b9d1f49cc6
ryanschuttig <ryanschuttig@gmail.com>
2020-09-03 13:55:00 -0400

    semantic ui test

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
-// Import React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
 
-// Create react component
-const App = function () {
-    const buttonText = "Submit";
-    const labelText = "Enter name:";
-
-    return (
-        <div>
-            <label className="label" htmlFor="name">
-                {labelText}
-            </label>
-            <input id="name" type="text" />
-            <button style={{ backgroundColor: "blue", color: "white" }}>
-                {buttonText}
-            </button>
-        </div>
-    );
+const App = () => {
+    return <div>Test</div>
 };
 
-// Take react component and show on page
-ReactDOM.render(
-    <App />,
-    document.querySelector("#root")
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
